494a0737db667937986e1c2e8cf8061d22635de9
NikolVasileva <nikol.vasileva9@gmail.com>
2025-09-14 00:26:56 +0300

    Add Basic HTTP Server

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ SoftUni JS Back-End Course Project
 1. Initialize Node.js Project
 2. Dev environment
 3. Debugging
+4. Add basic HTTP Server
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-console.log("Hello World21!")
-console.log("Hello World5!")
-console.log("Hello World3!")
+import http from "http"
+
+const server = http.createServer((req, res) => {
+    res.write("Hey");
+
+    res.end();
+});
+
+server.listen(6000);
+console.log("Server is listening to http://localhost:6000...")
+
